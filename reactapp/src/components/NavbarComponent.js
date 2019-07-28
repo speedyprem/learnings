f0d4666d46035c2c19515987f2c2783ae2c367bd
Prem Tiwari <prem.tiwari@news.co.uk>
2019-07-28 11:38:01 +0530

    Added router for navbar

--- reactapp/src/components/NavbarComponent.js
+++ reactapp/src/components/NavbarComponent.js
@@ -1,30 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function NavbarComponent() {
 	return (
 		<nav className="navbar navbar-expand-md navbar-dark bg-dark">
 			<div className="container">
-				<a className="navbar-brand" href="index.html">Simple Ecommerce</a>
+				<link className="navbar-brand" href="index.html">Simple Ecommerce</link>
 				<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
 					<span className="navbar-toggler-icon"></span>
 				</button>
-
 				<div className="collapse navbar-collapse justify-content-end" id="navbarsExampleDefault">
 					<ul className="navbar-nav m-auto">
 						<li className="nav-item">
-							<a className="nav-link" href="index.html">Home</a>
+							<link className="nav-link" href="index.html">Home</link>
 						</li>
 						<li className="nav-item">
-							<a className="nav-link" href="category.html">Categories <span className="sr-only">(current)</span></a>
+							<link className="nav-link" href="category.html">Categories <span className="sr-only">(current)</span></link>
 						</li>
 						<li className="nav-item">
-							<a className="nav-link" href="product.html">Product</a>
+							<link className="nav-link" href="product.html">Product</link>
 						</li>
 						<li className="nav-item">
-							<a className="nav-link" href="cart.html">Cart</a>
+							<link className="nav-link" href="cart.html">Cart</link>
 						</li>
 						<li className="nav-item">
-							<a className="nav-link" href="contact.html">Contact</a>
+							<link className="nav-link" href="/contact">Contact</link>
 						</li>
 					</ul>
 				</div>
